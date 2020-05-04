71c7788932f5fa7770db53b75973f8e03ea6f84e
Richard Dang <richardspacedang@gmail.com>
2020-05-04 02:41:31 -0400

    Finish 4.1 - 4.2

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const blogsRouter = require("./routes/blogs");
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connceted to MongoDB"))
+  .catch((err) => console.log("Failed to connect to MongoDB: ", err));
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
